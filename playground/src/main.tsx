f70a2a662e8805995ccc68e805253640da313f26
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2022-12-13 14:14:49 +0900

    custom main.tsx

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import ReactDOM from "react-dom/client";
+import App from "../../templates/auth/src/app";
+import Navigation from "./components/atom/navigation";
+import Routes from "./routes";
+import "../../templates/_shared/src/index.css";
 
-const App = () => <>React code</>;
+async function prepare() {
+  if (!import.meta.env.DEV) return;
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const { worker } = await import(
+    "../../templates/_shared/src/__tests__/mocks/api"
+  );
+
+  worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+prepare().then(() => {
+  return ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  ).render(
+    <React.StrictMode>
+      <App>
+        <Routes />
+        <Navigation />
+      </App>
+    </React.StrictMode>
+  );
+});
